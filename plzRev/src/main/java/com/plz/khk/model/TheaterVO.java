1c83a7ceb461e0164fd4020bc08c9f12010e1c68
kimhk <kimhk@DESKTOP-TT8D1GB>
2017-12-04 10:55:52 +0900

    theaterVO theaterscreeno -> theaterscreenno 변경

--- plzRev/src/main/java/com/plz/khk/model/TheaterVO.java
+++ plzRev/src/main/java/com/plz/khk/model/TheaterVO.java
@@ -7,87 +7,88 @@ public class TheaterVO {
 	private String theaterlocation;		// 극장위치
 	private String theatername;			// 극장이름
 	private String theaterintro;		// 극장소개
-	private int theaterscreeno;			// 관갯수
-	private int theaterlatitude;		// 극장위치위도
-	private int theaterlongitude;		// 극장위치경도
+	private int theaterscreenno;		// 관갯수
+	private double theaterlatitude;		// 극장위치위도
+	private double theaterlongitude;	// 극장위치경도
 	
 	public TheaterVO() { }
+
 	public TheaterVO(int theaterno, int fk_regionno, String theaterlocation, String theatername, String theaterintro,
-			int theaterscreeno, int theaterlatitude, int theaterlongitude) {
-	
+			int theaterscreenno, double theaterlatitude, double theaterlongitude) {
+		super();
 		this.theaterno = theaterno;
 		this.fk_regionno = fk_regionno;
 		this.theaterlocation = theaterlocation;
 		this.theatername = theatername;
 		this.theaterintro = theaterintro;
-		this.theaterscreeno = theaterscreeno;
+		this.theaterscreenno = theaterscreenno;
 		this.theaterlatitude = theaterlatitude;
 		this.theaterlongitude = theaterlongitude;
 	}
+
 	public int getTheaterno() {
 		return theaterno;
 	}
-	
+
 	public void setTheaterno(int theaterno) {
 		this.theaterno = theaterno;
 	}
-	
+
 	public int getFk_regionno() {
 		return fk_regionno;
 	}
-	
+
 	public void setFk_regionno(int fk_regionno) {
 		this.fk_regionno = fk_regionno;
 	}
-	
+
 	public String getTheaterlocation() {
 		return theaterlocation;
 	}
-	
+
 	public void setTheaterlocation(String theaterlocation) {
 		this.theaterlocation = theaterlocation;
 	}
-	
+
 	public String getTheatername() {
 		return theatername;
 	}
-	
+
 	public void setTheatername(String theatername) {
 		this.theatername = theatername;
 	}
-	
+
 	public String getTheaterintro() {
 		return theaterintro;
 	}
-	
+
 	public void setTheaterintro(String theaterintro) {
 		this.theaterintro = theaterintro;
 	}
-	
-	public int getTheaterscreeno() {
-		return theaterscreeno;
+
+	public int getTheaterscreenno() {
+		return theaterscreenno;
 	}
-	
-	public void setTheaterscreeno(int theaterscreeno) {
-		this.theaterscreeno = theaterscreeno;
+
+	public void setTheaterscreenno(int theaterscreenno) {
+		this.theaterscreenno = theaterscreenno;
 	}
-	
-	public int getTheaterlatitude() {
+
+	public double getTheaterlatitude() {
 		return theaterlatitude;
 	}
-	
-	public void setTheaterlatitude(int theaterlatitude) {
+
+	public void setTheaterlatitude(double theaterlatitude) {
 		this.theaterlatitude = theaterlatitude;
 	}
-	
-	public int getTheaterlongitude() {
+
+	public double getTheaterlongitude() {
 		return theaterlongitude;
 	}
-	
-	public void setTheaterlongitude(int theaterlongitude) {
+
+	public void setTheaterlongitude(double theaterlongitude) {
 		this.theaterlongitude = theaterlongitude;
 	}
-	
-	
+
 	
 }
